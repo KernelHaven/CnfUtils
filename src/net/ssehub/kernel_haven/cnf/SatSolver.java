03f81dde8c70bf373a7ce4eea16b1961408c650b
Adam <adam@sse.uni-hildesheim.de>
2017-09-06 11:10:05 +0200

    Use sat4j-core
    Remove commons-beanutils hack

--- src/net/ssehub/kernel_haven/cnf/SatSolver.java
+++ src/net/ssehub/kernel_haven/cnf/SatSolver.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 import org.sat4j.core.Vec;
 import org.sat4j.core.VecInt;
-import org.sat4j.maxsat.SolverFactory;
+import org.sat4j.minisat.SolverFactory;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.ISolver;
 import org.sat4j.specs.IVec;
