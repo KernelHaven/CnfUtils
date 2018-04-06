47ada6129836d6a17773b6c9bf6bc8a486f42513
adam-sse <adam@sse.uni-hildesheim.de>
2018-04-06 15:21:33 +0200

    Refactor SimplifyingDisjunctionQueue
    This may fix a bug where current was always added to previous

--- src/net/ssehub/kernel_haven/logic_utils/SimplifyingDisjunctionQueue.java
+++ src/net/ssehub/kernel_haven/logic_utils/SimplifyingDisjunctionQueue.java
@@ -25,11 +25,18 @@ import net.ssehub.kernel_haven.util.null_checks.Nullable;
  */
 public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
 
+    private @NonNull SatSolver solver;
+    
+    private @NonNull IFormulaToCnfConverter converter;
+    
     /**
      * Creates a new {@link SimplifyingDisjunctionQueue}.
      */
     public SimplifyingDisjunctionQueue() {
         super(true);
+        
+        solver = new SatSolver();
+        converter = FormulaToCnfConverterFactory.create(Strategy.RECURISVE_REPLACING);
     }
 
     @Override
@@ -40,11 +47,6 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
         if (isTrue || queue.isEmpty()) {
             result = True.INSTANCE;
         } else {
-            
-            
-            SatSolver solver = new SatSolver();
-            IFormulaToCnfConverter converter = FormulaToCnfConverterFactory.create(Strategy.RECURISVE_REPLACING);
-            
             // all previously considered formulas ORd together (used in sat calls). 
             Formula previous = notNull(queue.poll());
             
@@ -53,40 +55,17 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
             helperQueue.add(previous);
             
             for (Formula current : queue) {
+                RelevancyType relevancy;
                 try {
-                    
-                    // two sat() calls to check if previous, current or both need to be considered
-                    
-                    // sat(!previous AND current)
-                    // false -> current is subset of previous -> ignore current (nothing to do)
-                    if (solver.isSatisfiable(converter.convert(and(not(previous), current)))) {
-                        // true -> current is not subset of previous
-                        
-                        // sat(previous AND !current)
-                        if (solver.isSatisfiable(converter.convert(and(previous, not(current))))) {
-                            // neither previous nor current are subsets of each other -> consider both (add current)
-                            previous = new Disjunction(previous, current);
-                            helperQueue.add(current);
-                            
-                        } else {
-                            // false -> previous is subset of current -> ignore previous (consider only current)
-                            previous = current;
-                            helperQueue.reset();
-                            helperQueue.add(current);
-                        }
-                        
-                    }
-                    
-                    previous = new Disjunction(previous, current);
+                    relevancy = checkRelevancy(previous, current);
                     
                 } catch (StackOverflowError e) {
                     LOGGER.logWarning(varName + " has a PC that is too deep:",
                             "Current: " + new DepthCalculator().visit(current),
                             "Previous: " + new DepthCalculator().visit(previous));
                     
-                    // add current to be safe
-                    previous = new Disjunction(previous, current);
-                    helperQueue.add(current);
+                    // consider both, to be safe
+                    relevancy = RelevancyType.BOTH_RELEVANT;
                     
                 } catch (ConverterException | SolverException e) {
                     if (null != varName) {
@@ -95,18 +74,101 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
                         LOGGER.logExceptionWarning("Error while creating disjunction", e);
                     }
                     
-                    // add current to be safe
+                    // consider both, to be safe
+                    relevancy = RelevancyType.BOTH_RELEVANT;
+                }
+                    
+                switch (relevancy) {
+                case PREVIOUS_RELEVANT:
+                    // consider only previous; current is ignored (do nothing)
+                    break;
+                    
+                case CURRENT_RELEVANT:
+                    // consider only current; previous is overridden
+                    previous = current;
+                    helperQueue.reset();
+                    helperQueue.add(current);
+                    break;
+                    
+                case BOTH_RELEVANT:
+                    // add current
                     previous = new Disjunction(previous, current);
                     helperQueue.add(current);
+                    break;
+                    
+                default:
+                    throw new RuntimeException("Invalid relevancy: " + relevancy); // can't happen
                 }
             }
             
             result = helperQueue.getDisjunction(varName);
-            
         }
         
         // Reset
         reset();
+        
+        return result;
+    }
+    
+    /**
+     * Which formulas are relevant.
+     */
+    private static enum RelevancyType {
+        
+        /**
+         * Both, current and previous, need to be considered.
+         */
+        BOTH_RELEVANT,
+        
+        /**
+         * Only previous needs to be considered; current is a subset of previous.
+         */
+        PREVIOUS_RELEVANT,
+        
+        /**
+         * Only current needs to be considered; previous is a subset of current.
+         */
+        CURRENT_RELEVANT;
+        
+    }
+    
+    /**
+     * Checks which of the given {@link Formula}s is relevant.
+     * 
+     * @param previous All previously relevant formulas, OR'd together.
+     * @param current The new formula that will (possibly) be added.
+     * 
+     * @return Which formulas are relevant.
+     * 
+     * @throws ConverterException If CNF conversion fails.
+     * @throws SolverException If the SAT solver fails. 
+     */
+    private @NonNull RelevancyType checkRelevancy(@NonNull Formula previous, @NonNull Formula current)
+            throws SolverException, ConverterException {
+        
+        RelevancyType result;
+        
+        // two sat() calls to check if previous, current or both need to be considered
+        
+        // sat(!previous AND current)
+        if (solver.isSatisfiable(converter.convert(and(not(previous), current)))) {
+            // true -> current is not subset of previous
+            
+            // sat(previous AND !current)
+            if (solver.isSatisfiable(converter.convert(and(previous, not(current))))) {
+                // neither previous nor current are subsets of each other -> consider both (add current)
+                result = RelevancyType.BOTH_RELEVANT;
+                
+            } else {
+                // false -> previous is subset of current -> ignore previous (consider only current)
+                result = RelevancyType.CURRENT_RELEVANT;
+            }
+            
+        } else {
+            // false -> current is subset of previous -> ignore current (nothing to do)
+            result = RelevancyType.PREVIOUS_RELEVANT;
+        }
+        
         
         return result;
     }
