af716bf5b3dc2c31f01602aba1062aabc0fb6cdc
Elscha <elscha@sse.uni-hildesheim.de>
2018-04-10 11:26:50 +0200

    Experimental analysis of sub formulas moved to right position in code.

--- src/net/ssehub/kernel_haven/logic_utils/SimplifyingDisjunctionQueue.java
+++ src/net/ssehub/kernel_haven/logic_utils/SimplifyingDisjunctionQueue.java
@@ -74,13 +74,12 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
         } else {
             // all previously considered formulas ORd together (used in sat calls). 
             Formula previous = notNull(queue.poll());
-            
             // a helper queue to create a balanced result; all considered formulas are added to it
             DisjunctionQueue helperQueue = new DisjunctionQueue(false);
             helperQueue.add(previous);
             
             for (Formula current : queue) {
-                RelevancyType relevancy;
+                IRelevancyType relevancy;
                 try {
                     relevancy = checkRelevancy(previous, current);
                     
@@ -94,27 +93,38 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
                     // consider both, to be safe
                     relevancy = RelevancyType.BOTH_RELEVANT;
                 }
-                    
-                switch (relevancy) {
-                case PREVIOUS_RELEVANT:
-                    // consider only previous; current is ignored (do nothing)
-                    break;
-                    
-                case CURRENT_RELEVANT:
-                    // consider only current; previous is overridden
-                    previous = current;
-                    helperQueue.reset();
-                    helperQueue.add(current);
-                    break;
-                    
-                case BOTH_RELEVANT:
-                    // add current
-                    previous = new Disjunction(previous, current);
-                    helperQueue.add(current);
-                    break;
-                    
-                default:
-                    throw new RuntimeException("Invalid relevancy: " + relevancy); // can't happen
+                
+                if (relevancy instanceof RelevancyType) {
+                    switch ((RelevancyType) relevancy) {
+                    case PREVIOUS_RELEVANT:
+                        // consider only previous; current is ignored (do nothing)
+                        break;
+                        
+                    case CURRENT_RELEVANT:
+                        // consider only current; previous is overridden
+                        previous = current;
+                        helperQueue.reset();
+                        helperQueue.add(current);
+                        break;
+                        
+                    case BOTH_RELEVANT:
+                        // add current
+                        previous = new Disjunction(previous, current);
+                        helperQueue.add(current);
+                        break;
+                        
+                    default:
+                        throw new RuntimeException("Invalid relevancy: " + relevancy); // can't happen
+                    }
+                } else if (relevancy instanceof SubRelevance) {
+                    // add sub formula of current
+                    SubRelevance subRelevance = (SubRelevance) relevancy;
+                    Formula relevantPart = subRelevance.relevantSubFormula;
+                    previous = new Disjunction(previous, relevantPart);
+                    helperQueue.add(relevantPart);
+                } else {
+                    // can't happen
+                    throw new RuntimeException("Invalid relevancy class: " + relevancy.getClass().getCanonicalName());
                 }
             }
             
@@ -123,14 +133,22 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
         
         // Reset
         reset();
-        
         return result;
+    }
+    
+    /**
+     * Markup interface.
+     * @author El-Sharkawy
+     *
+     */
+    private static interface IRelevancyType {
+        
     }
     
     /**
      * Which formulas are relevant.
      */
-    private static enum RelevancyType {
+    private static enum RelevancyType implements IRelevancyType {
         
         /**
          * Both, current and previous, need to be considered.
@@ -150,6 +168,26 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
     }
     
     /**
+     * Marks a sub formula to be relevant, while the eliminated formula is irrelevant.
+     * @author El-Sharkawy
+     *
+     */
+    private static class SubRelevance implements IRelevancyType {
+        /**
+         * The remaining relevant part of a formula.
+         */
+        private @NonNull Formula relevantSubFormula;
+        
+        /**
+         * Sole constructor.
+         * @param relevantSubFormula The relevant part of a bigger formula which shall be kept.
+         */
+        private SubRelevance(@NonNull Formula relevantSubFormula) {
+            this.relevantSubFormula = relevantSubFormula;
+        }
+    }
+    
+    /**
      * Checks which of the given {@link Formula}s is relevant.
      * 
      * @param previous All previously relevant formulas, OR'd together.
@@ -160,10 +198,10 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
      * @throws ConverterException If CNF conversion fails.
      * @throws SolverException If the SAT solver fails. 
      */
-    private @NonNull RelevancyType checkRelevancy(@NonNull Formula previous, @NonNull Formula current)
+    private @NonNull IRelevancyType checkRelevancy(@NonNull Formula previous, @NonNull Formula current)
             throws SolverException, ConverterException {
         
-        RelevancyType result;
+        IRelevancyType result = null;
         
         // two sat() calls to check if previous, current or both need to be considered
         
@@ -183,9 +221,87 @@ public class SimplifyingDisjunctionQueue extends DisjunctionQueue {
             
         } else {
             // false -> current is subset of previous -> ignore current
-            result = RelevancyType.PREVIOUS_RELEVANT;
+            
+            // Check if a sub formula is covered by previous 
+            if (current instanceof Disjunction) {
+                Formula left = ((Disjunction) current).getLeft();
+                Formula right = ((Disjunction) current).getRight();
+                
+                result = checkSubRelevancy(previous, left, right);
+                if (null == result) {
+                    result = checkSubRelevancy(previous, right, left);
+                }
+            } else if (current instanceof Negation && ((Negation) current).getFormula() instanceof Conjunction) {
+                // Transform: !(A AND B) into !A OR !B
+                Conjunction inner = (Conjunction) ((Negation) current).getFormula();
+                Formula left = inner.getLeft();
+                left = (left instanceof Negation) ? ((Negation) left).getFormula() : new Negation(left);
+                Formula right = inner.getRight();
+                right = (right instanceof Negation) ? ((Negation) right).getFormula() : new Negation(right);
+                
+                result = checkSubRelevancy(previous, left, right);
+                if (null == result) {
+                    result = checkSubRelevancy(previous, right, left);
+                }
+            }
+            
+            if (null == result) {
+                result = RelevancyType.PREVIOUS_RELEVANT;
+            }
         }
         
+        
+        return result;
+    }
+    
+    /**
+     * Checks which of the given {@link Formula}s is relevant. recursive function, which aborts after the first match.
+     * 
+     * @param previous All previously relevant formulas, OR'd together.
+     * @param current A sub element of the new formula that will (possibly) be added (will be disjunction-wise split).
+     * @param base The other part of current, which must be added in case that current is irrelevant.
+     * 
+     * @return A relevant sub formula of current or <tt>null</tt> if the whole formula is relevant
+     * 
+     * @throws ConverterException If CNF conversion fails.
+     * @throws SolverException If the SAT solver fails. 
+     */
+    private @Nullable SubRelevance checkSubRelevancy(@NonNull Formula previous, @NonNull Formula current,
+        @NonNull Formula base) throws SolverException, ConverterException {
+        
+        // Here we check only if the current formula (which covers a smaller configuration space) is covered by previous
+        SubRelevance result = null;
+        
+        // sat(!previous AND current)
+        if (solver.isSatisfiable(converter.convert(and(not(previous), current)))) {
+            // true -> current is not subset of previous: continue recursion if possible
+            
+            // Recursion
+            if (current instanceof Disjunction) {
+                Formula left = ((Disjunction) current).getLeft();
+                Formula right = ((Disjunction) current).getRight();
+                
+                result = checkSubRelevancy(previous, left, right);
+                if (null == result) {
+                    result = checkSubRelevancy(previous, right, left);
+                }
+            } else if (current instanceof Negation && ((Negation) current).getFormula() instanceof Conjunction) {
+                // Transform: !(A AND B) into !A OR !B
+                Conjunction inner = (Conjunction) ((Negation) current).getFormula();
+                Formula left = inner.getLeft();
+                left = (left instanceof Negation) ? ((Negation) left).getFormula() : new Negation(left);
+                Formula right = inner.getRight();
+                right = (right instanceof Negation) ? ((Negation) right).getFormula() : new Negation(right);
+                
+                result = checkSubRelevancy(previous, left, right);
+                if (null == result) {
+                    result = checkSubRelevancy(previous, right, left);
+                }
+            }
+        } else {
+            // false -> current is subset of previous -> ignore current, keep base
+            result = new SubRelevance(base);
+        }
         
         return result;
     }
