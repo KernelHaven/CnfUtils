57715866553f3593e5cc1d73b87a681055a69fb0
Adam <adam@sse.uni-hildesheim.de>
2019-04-28 11:42:34 +0200

    Remove unnecessary SuppressWarnings

--- test/net/ssehub/kernel_haven/cnf/VmToCnfConverterTest.java
+++ test/net/ssehub/kernel_haven/cnf/VmToCnfConverterTest.java
@@ -54,7 +54,6 @@ public class VmToCnfConverterTest {
     public void testCompleteVariableUse() throws FormatException {
         // Preconditions
         Set<VariabilityVariable> set = new HashSet<VariabilityVariable>();
-        @SuppressWarnings("null")
         @NonNull String[] variables = {"ALPHA", "ALPHA_MODULE", "GAMMA", "BETA_MODULE", "BETA"};
         for (int i = 0; i < variables.length; i++) {
             set.add(new VariabilityVariable(variables[i], "bool", i + 1));
